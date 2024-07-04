2b313e5011c90943956ffa1b8a88b6026bf9d09e
lemaj42 <abdelmadjidchabane89@gmail.com>
2024-07-04 14:51:07 +0200

    mise a jour

--- src/components/ContactSection.js
+++ src/components/ContactSection.js
@@ -9,36 +9,12 @@ const ContactSection = () => {
                 <h6 className="card-subtitle mb-2 text-muted" style={{ color: 'black' }}>Développeur web Junior</h6>
             </div>
             <div className="container">
-                <div className="row">
-                    <div className="col">
-                        <div className="card">
-                            <div className="card-header bg-black text-white"><i className="fa fa-envelope"></i> Contactez-moi.</div>
-                            <div className="card-body">
-                                <form action="./process/process_mail.php" id="contactForm" method="post">
-                                    <div className="form-group">
-                                        <label htmlFor="name">Votre nom</label>
-                                        <input type="text" id="name" name="name" required className="form-control" placeholder="Votre nom" />
-                                    </div>
-                                    <div className="form-group">
-                                        <label htmlFor="email">Votre Email</label>
-                                        <input type="email" id="email" name="email" required className="form-control" placeholder="Votre Email" />
-                                    </div>
-                                    <div className="form-group">
-                                        <label htmlFor="message">Message</label>
-                                        <textarea id="message" name="message" rows="4" required className="form-control" placeholder="Message"></textarea>
-                                    </div>
-                                    <div className="mx-auto text-center">
-                                        <button type="submit" className="btn btn-dark text-right">Envoyer</button>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
+                <div className="row justify-content-center">
+
                     <div className="col-12 col-sm-4">
                         <div className="card bg-light mb-3">
                             <div className="card-header bg-black text-white text-uppercase"><i className="fa fa-home"></i> Adresse</div>
                             <div className="card-body">
-                                <p>30 allée Paulette loire</p>
                                 <p>42153, Riorges</p>
                                 <p>France</p>
                                 <p>Email : abdelmadjidchabane89@gmail.com</p>
